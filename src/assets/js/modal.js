5c8953fbec69e04281be1b36484f17bfee991b92
RustamBotobaev <dejavu1510@gmail.com>
2021-01-18 20:12:44 +0300

    added js file

---
+++ src/assets/js/modal.js
@@ -0,0 +1,41 @@
+const modalBtn = document.querySelectorAll('[data-modal]');
+const body = document.body;
+const modalClose = document.querySelectorAll('.modal__close');
+
+modalBtn.forEach(item => {
+    item.addEventListener('click', event => {
+        let $this = event.currentTarget;
+        let modalId = $this.getAttribute('data-modal');
+        let modal = document.getElementById(modalId);
+        let modalContent = modal.querySelector('.modal__content');
+
+        modalContent.addEventListener('click', event => {
+            event.stopPropagation();
+        });
+
+        modal.classList.add('show');
+        body.classList.add('no-scroll');
+
+    });
+});
+
+modalClose.forEach(item => {
+    item.addEventListener('click', event => {
+        let currentModal = event.target.closest('.modal');
+
+        closeModal(currentModal);
+    });
+});
+
+modal.forEach(item => {
+    item.addEventListener('click', event => {
+        let currentModal = event.target;
+
+        closeModal(currentModal);
+    });
+});
+
+function closeModal(currentModal) {
+    currentModal.classList.remove('show');
+    body.classList.remove('no-scroll');
+}
